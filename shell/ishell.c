33263c77ef38b8e28fae488bc0266a305cd8b88f
Troy Schwab <trs016@bucknell.edu>
2017-12-09 15:17:16 -0500

    added commands history, previous, and exit

--- shell/ishell.c
+++ shell/ishell.c
@@ -1,5 +1,3 @@
-// extra feature added was indefinite commands per line separated by semicolons as opposed to two
-
 #include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,25 +10,57 @@
 int main(void) {
     char buf[MAXLINE];
     char *command[MAXLINE]; 
-    char *semi; 
+    char *semi;
+    char prev[MAXLINE]; 
+    char *log[30];
+    int iteration = -1;
     printf("ishell> ");
     while(fgets(buf, MAXLINE, stdin) != NULL) {
+        iteration++;
         if (buf[strlen(buf) - 1] == '\n') {
             buf[strlen(buf) - 1] = 0;
+            log[iteration] = malloc(sizeof(buf));
+            log[iteration] = strcpy(log[iteration], buf);
             }
-        if ((buf[0] == '\t') && (buf[1] == '\t')) { 
+        if (strcmp(buf, "exit") == 0) {
+            run("echo ");
+            break;
+        }
+        /*
+         * adding funcionality where !n executes the nth command
+         *
+        if ((buf[0] == '!') && ((int) buf[1] >= (int) '0') && ((int) buf[1] <= (int) '9')) {
+            int n = atoi(&buf[1]);
+            printf("n %d\n", n);
+            printf("comm %s\n", log[n]);
+            run(log[n]);
+        }
+        */
+        if (strcmp(buf, "history") == 0) {
+            int i = 0;
+            while(log[i] != NULL) {
+                printf("%s\n", log[i]);
+                i++;
+            }
+            run("echo ");
+        } else if (strcmp(buf, "previous") == 0) {
+            run(prev);
+        } else if ((buf[0] == '\t') && (buf[1] == '\t')) { 
             run("ls");
+            strcpy(prev, "ls");
         } else {
             int i = 0;
             semi = ";";
-            command[i] = strtok(buf, semi);
+            command[i] = strtok(buf, semi); 
             run(*command);
+            strcpy(prev, *command);
             command[i] = strtok(NULL, semi);
             while (*command != NULL) {
                 run(*command);
+                strcpy(prev, *command);
                 command[i] = strtok(NULL, semi);
             }
-        }
+        } 
         printf("ishell> ");
         }
     exit(0);
@@ -53,7 +83,7 @@ void run(char buf[MAXLINE]) {
             exit(-1);
         }
         exit(0);
-    } 
+    }
     int status;
     pid = Waitpid(pid, &status, 0);
     const int error = WEXITSTATUS(status);
--- shell/ishell.h
+++ shell/ishell.h
@@ -1,6 +1,8 @@
 #ifndef ishell
 #define ishell
+
 #define MAXLINE 25
+#define LOG 30
 
 void run(char buf[MAXLINE]);
 
